2ecc6c19134c1e550e9388339cad9ea4e160dfe1
Hoang Nguyen <100697734+isoamyl50@users.noreply.github.com>
2024-05-20 13:58:45 +0700

    Added functionality for generating and validating OTP for new users

--- src/main/java/com/example/VieTicketSystem/model/service/OTPService.java
+++ src/main/java/com/example/VieTicketSystem/model/service/OTPService.java
@@ -1,5 +1,9 @@
 package com.example.VieTicketSystem.model.service;
 
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.jboss.aerogear.security.otp.Totp;
 import org.jboss.aerogear.security.otp.api.Clock;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +12,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class OTPService {
     private final int timeWindow;
+
+    private static final Map<String, String> newUserOTPs = new ConcurrentHashMap<>();
+
+    public String generateNewUserOTP(String email) {
+        String secretKey = UUID.randomUUID().toString();
+        String otp = generateOTP(secretKey);
+        newUserOTPs.put(email, otp);
+        return otp;
+    }
+
+    public boolean validateNewUserOTP(String email, String userOTP) {
+        String otp = newUserOTPs.get(email);
+        if (otp == null) {
+            throw new RuntimeException("No OTP found for email");
+        }
+        newUserOTPs.remove(email);
+        return otp.equals(userOTP);
+    }
 
     public OTPService(@Value("${OTP_TIME_WINDOW_MINUTES}") double timeWindow) {
         timeWindow *= 60; // Convert minutes to seconds
