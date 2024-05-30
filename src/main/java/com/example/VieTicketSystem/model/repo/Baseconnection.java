3b48b0ac42c1b3dcea50d36247fcd8574dc4140b
hulaun <vangolahung4@gmail.com>
2024-05-30 15:03:51 +0700

    update

--- src/main/java/com/example/VieTicketSystem/model/repo/Baseconnection.java
+++ src/main/java/com/example/VieTicketSystem/model/repo/Baseconnection.java
@@ -3,7 +3,7 @@ package com.example.VieTicketSystem.model.repo;
 public class Baseconnection {
     static String url = "jdbc:mysql://localhost:3306/VieTicket";
     static String username = "root";
-    static String password = "thanhvinh";
+    static String password = "2Bthere4u";
     static String nameClass = "com.mysql.cj.jdbc.Driver";
 
 }
