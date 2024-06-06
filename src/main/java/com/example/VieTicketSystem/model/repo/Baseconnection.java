62bec252da4f486aded013f5467f059d2e78da10
Hoang Nguyen <100697734+isoamyl50@users.noreply.github.com>
2024-06-06 15:58:32 +0700

    Update database connection URL and credentials in Baseconnection.java

--- src/main/java/com/example/VieTicketSystem/model/repo/Baseconnection.java
+++ src/main/java/com/example/VieTicketSystem/model/repo/Baseconnection.java
@@ -5,14 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Baseconnection {
-<<<<<<< HEAD
-    static String url = "jdbc:mysql://localhost:3306/VieTicket1";
-=======
-    static String url = "jdbc:mysql://localhost:3307/VieTicket1";
->>>>>>> 5e4c577 (search)
-    static String username = "root";
-    static String password = "thanhvinh";
-    static String nameClass = "com.mysql.cj.jdbc.Driver";
+    public static String url = "jdbc:mysql://localhost:3307/VieTicket1";
+    public static String username = "root";
+    public static String password = "thanhvinh";
+    public static String nameClass = "com.mysql.cj.jdbc.Driver";
     
     public static Connection getConnection() throws SQLException {
         try {
