b8871f61030b4cccacd5534db2a4bc48a780808e
vinh <dvih003@gmail.com>
2024-06-07 12:37:15 +0700

    update view detail

--- src/main/java/com/example/VieTicketSystem/model/repo/Baseconnection.java
+++ src/main/java/com/example/VieTicketSystem/model/repo/Baseconnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Baseconnection {
-    public static String url = "jdbc:mysql://localhost:3307/VieTicket1";
+    public static String url = "jdbc:mysql://localhost:3306/VieTicket1";
     public static String username = "root";
     public static String password = "thanhvinh";
     public static String nameClass = "com.mysql.cj.jdbc.Driver";
