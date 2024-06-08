68a5c8d569d896a1bd1080961b785751826491cb
vinh <dvih003@gmail.com>
2024-06-08 15:17:06 +0700

    fix buy ticket

--- src/main/java/com/example/VieTicketSystem/model/service/FileUploadImpl.java
+++ src/main/java/com/example/VieTicketSystem/model/service/FileUploadImpl.java
@@ -1,6 +1,7 @@
 package com.example.VieTicketSystem.model.service;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.Transformation;
 import com.cloudinary.utils.ObjectUtils;
 
 import lombok.RequiredArgsConstructor;
@@ -13,22 +14,35 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class FileUploadImpl implements FileUpload{
+public class FileUploadImpl implements FileUpload {
 
-    private final  Cloudinary cloudinary;
-    @Override
-    public String uploadFile(MultipartFile multipartFile) throws IOException {
-        return cloudinary.uploader()
-                .upload(multipartFile.getBytes(),
-                        Map.of("public_id", UUID.randomUUID().toString()))
-                .get("url")
-                .toString();
-    }
-    public String uploadFileImgBannerAndPoster(MultipartFile multipartFile, int width, int height) throws IOException {
-        Map uploadResult = cloudinary.uploader().upload(multipartFile.getBytes(),
-                ObjectUtils.asMap(
-                        "transformation", new com.cloudinary.Transformation().width(width).height(height).crop("fill")
-                ));
-        return uploadResult.get("url").toString();
-    }
+        private final Cloudinary cloudinary;
+
+        @Override
+        public String uploadFile(MultipartFile multipartFile) throws IOException {
+                return cloudinary.uploader()
+                                .upload(multipartFile.getBytes(),
+                                                Map.of("public_id", UUID.randomUUID().toString()))
+                                .get("url")
+                                .toString();
+        }
+
+        public String uploadFileImgBannerAndPoster(MultipartFile multipartFile, int width, int height)
+                        throws IOException {
+                Map uploadResult = cloudinary.uploader().upload(multipartFile.getBytes(),
+                                ObjectUtils.asMap(
+                                                "transformation", new com.cloudinary.Transformation().width(width)
+                                                                .height(height).crop("fill")));
+                return uploadResult.get("url").toString();
+        }
+
+        public String uploadFileSeatMap(MultipartFile multipartFile) throws IOException {
+
+                Map uploadResult = cloudinary.uploader().upload(multipartFile.getBytes(),
+                                ObjectUtils.asMap(
+                                                "transformation", new Transformation().effect("trim")));
+
+                return uploadResult.get("url").toString();
+
+        }
 }
