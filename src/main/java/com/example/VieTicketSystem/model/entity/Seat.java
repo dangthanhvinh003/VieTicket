0593ef743664f3478f87282366315e9bbe55745e
Hoang Nguyen <100697734+isoamyl50@users.noreply.github.com>
2024-06-09 18:50:23 +0700

    chore: Update Seat class to use enum for taken status

--- src/main/java/com/example/VieTicketSystem/model/entity/Seat.java
+++ src/main/java/com/example/VieTicketSystem/model/entity/Seat.java
@@ -14,10 +14,35 @@ public class Seat {
     private int seatId;
     private String number;
     private float ticketPrice;
-    private boolean isTaken;
+    private TakenStatus taken;
     private Row row;
 
-    private enum TakenStatus {
-        TAKEN, PENDING, NOT_TAKEN
+    public boolean isAvailable() {
+        return taken == TakenStatus.AVAILABLE;
+    }
+
+    public enum TakenStatus {
+        AVAILABLE(0),
+        TAKEN(1),
+        RESERVED(2);
+
+        private final int value;
+
+        private TakenStatus(int value) {
+            this.value = value;
+        }
+
+        public int toInteger() {
+            return value;
+        }
+
+        public static TakenStatus fromInteger(int value) {
+            return switch (value) {
+                case 0 -> AVAILABLE;
+                case 1 -> TAKEN;
+                case 2 -> RESERVED;
+                default -> null;
+            };
+        }
     }
 }
