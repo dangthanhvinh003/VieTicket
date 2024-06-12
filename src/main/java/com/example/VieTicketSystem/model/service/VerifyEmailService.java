61aee2bae9764ce40788993015716110db37df4d
Hoang Nguyen <100697734+isoamyl50@users.noreply.github.com>
2024-06-12 17:06:57 +0700

    chore: Update password reset controller endpoints and add request mapping to auth API

--- src/main/java/com/example/VieTicketSystem/model/service/VerifyEmailService.java
+++ src/main/java/com/example/VieTicketSystem/model/service/VerifyEmailService.java
@@ -2,7 +2,6 @@ package com.example.VieTicketSystem.model.service;
 
 import java.time.LocalDateTime;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
