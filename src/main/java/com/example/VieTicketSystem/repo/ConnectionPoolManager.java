eca29367e1b52248c8c5fcb351784194e7ceac80
Hoang Nguyen <100697734+isoamyl50@users.noreply.github.com>
2024-07-08 10:22:59 +0700

    chore: Add ConnectionPoolManager for managing database connections

---
+++ src/main/java/com/example/VieTicketSystem/repo/ConnectionPoolManager.java
@@ -0,0 +1,45 @@
+package com.example.VieTicketSystem.repo;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Component
+public class ConnectionPoolManager {
+    private static final HikariConfig config = new HikariConfig();
+    @Getter
+    private static HikariDataSource dataSource;
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
+    @PostConstruct
+    public void initializeDataSource() {
+        config.setJdbcUrl(Baseconnection.url);
+        config.setUsername(Baseconnection.username);
+        config.setPassword(Baseconnection.password);
+        config.setMaximumPoolSize(32);
+        dataSource = new HikariDataSource(config);
+    }
+
+    private ConnectionPoolManager() {
+    }
+
+    @Bean
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
