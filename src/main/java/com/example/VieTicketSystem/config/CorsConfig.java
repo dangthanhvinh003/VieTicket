78d04906d7af86f522bbb70e45f637a7d67f52f4
khanh <dt.khanh3007@gmail.com>
2024-07-09 23:25:06 +0700

    clear-unnescessary

--- src/main/java/com/example/VieTicketSystem/config/CorsConfig.java
+++ src/main/java/com/example/VieTicketSystem/config/CorsConfig.java
@@ -1,7 +1,6 @@
 package com.example.VieTicketSystem.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
