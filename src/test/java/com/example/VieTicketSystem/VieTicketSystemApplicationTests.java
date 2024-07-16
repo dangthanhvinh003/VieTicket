d3d4f357022a2077b5e0471da2fc2ca8dfd79a71
hulaun <vangolahung4@gmail.com>
2024-07-16 21:46:22 +0700

    add some tests for create event

--- src/test/java/com/example/VieTicketSystem/VieTicketSystemApplicationTests.java
+++ src/test/java/com/example/VieTicketSystem/VieTicketSystemApplicationTests.java
@@ -306,6 +306,149 @@ class VieTicketSystemApplicationTests {
 		assertTrue(dashboardHeader.isDisplayed());
 	}
 
+	// @Test
+	// public void testStartDateNull() {
+	// driver.get("http://localhost:8080/auth/login");
+	// WebElement username = driver.findElement(By.id("username"));
+	// username.sendKeys("o1");
+	// WebElement password = driver.findElement(By.id("password"));
+	// password.sendKeys("123");
+	// WebElement loginButton =
+	// driver.findElement(By.xpath("//button[text()='Login']"));
+	// loginButton.click();
+	// WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+	// WebElement createEventLink = wait.until(
+	// ExpectedConditions.presenceOfElementLocated(By.cssSelector("a.btn.btn-outline-success[href='/createEvent']")));
+	// createEventLink.click();
+	// WebElement bannerUpload = driver.findElement(By.name("banner"));
+	// bannerUpload.sendKeys("C:\\Users\\ACER\\Downloads\\haizz.png");
+	// WebElement posterUpload = driver.findElement(By.name("poster"));
+	// posterUpload.sendKeys("C:\\Users\\ACER\\Downloads\\haizz.png");
+	// WebElement nameInput = driver.findElement(By.id("name"));
+	// nameInput.sendKeys("Random Event Name");
+	// WebElement locationInput = driver.findElement(By.id("location"));
+	// locationInput.sendKeys("Random Location");
+	// WebElement endDateInput = driver.findElement(By.id("end_date"));
+	// endDateInput.clear();
+	// endDateInput.sendKeys("07202024\t190000");
+	// Select typeSelect = new Select(driver.findElement(By.id("type")));
+	// typeSelect.selectByValue("music");
+	// WebElement ticketSaleDateInput =
+	// driver.findElement(By.id("ticket_sale_date"));
+	// ticketSaleDateInput.clear();
+	// ticketSaleDateInput.sendKeys("07202024150000");
+	// WebElement ckEditor =
+	// driver.findElement(By.className("ck-editor__editable"));
+	// ckEditor.sendKeys("randomKeys");
+	// ckEditor.sendKeys(Keys.ENTER); // Example: Press Enter key
+	// WebElement createEventButton =
+	// driver.findElement(By.xpath("//input[@type='submit' and @class='btn
+	// btn-primary']"));
+	// ((JavascriptExecutor) driver).executeScript("arguments[0].click();",
+	// createEventButton);
+	// wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input:invalid")));
+	// WebElement invalidElement =
+	// driver.findElement(By.cssSelector("input:invalid"));
+	// String validationMessage = invalidElement.getAttribute("validationMessage");
+	// assertEquals(validationMessage, "Please fill out this field.");
+	// }
+
+	// @Test
+	// public void testEndDateValidation() {
+	// driver.get("http://localhost:8080/auth/login");
+	// WebElement username = driver.findElement(By.id("username"));
+	// username.sendKeys("o1");
+	// WebElement password = driver.findElement(By.id("password"));
+	// password.sendKeys("123");
+	// WebElement loginButton =
+	// driver.findElement(By.xpath("//button[text()='Login']"));
+	// loginButton.click();
+	// WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+	// WebElement createEventLink = wait.until(
+	// ExpectedConditions.presenceOfElementLocated(By.cssSelector("a.btn.btn-outline-success[href='/createEvent']")));
+	// createEventLink.click();
+	// WebElement bannerUpload = driver.findElement(By.name("banner"));
+	// bannerUpload.sendKeys("C:\\Users\\ACER\\Downloads\\haizz.png");
+	// WebElement posterUpload = driver.findElement(By.name("poster"));
+	// posterUpload.sendKeys("C:\\Users\\ACER\\Downloads\\haizz.png");
+	// WebElement nameInput = driver.findElement(By.id("name"));
+	// nameInput.sendKeys("Random Event Name");
+	// WebElement locationInput = driver.findElement(By.id("location"));
+	// locationInput.sendKeys("Random Location");
+	// WebElement startDateInput = driver.findElement(By.id("start_date"));
+	// startDateInput.clear();
+	// startDateInput.sendKeys("07202024\t140000");
+	// WebElement endDateInput = driver.findElement(By.id("end_date"));
+	// endDateInput.clear();
+	// endDateInput.sendKeys("07202024\t130000");
+	// Select typeSelect = new Select(driver.findElement(By.id("type")));
+	// typeSelect.selectByValue("music");
+	// WebElement ticketSaleDateInput =
+	// driver.findElement(By.id("ticket_sale_date"));
+	// ticketSaleDateInput.clear();
+	// ticketSaleDateInput.sendKeys("07202024150000");
+	// WebElement ckEditor =
+	// driver.findElement(By.className("ck-editor__editable"));
+	// ckEditor.sendKeys("randomKeys");
+	// ckEditor.sendKeys(Keys.ENTER); // Example: Press Enter key
+	// WebElement createEventButton =
+	// driver.findElement(By.xpath("//input[@type='submit' and @class='btn
+	// btn-primary']"));
+	// ((JavascriptExecutor) driver).executeScript("arguments[0].click();",
+	// createEventButton);
+	// WebElement alertMessage = driver.findElement(By.id("alertMessage"));
+	// String alertText = alertMessage.getText();
+	// assertEquals(alertText, "End Date must be in the present or future and not
+	// earlier than the Start Date.");
+	// }
+
+	// @Test
+	// public void testEndDateEmpty() {
+	// driver.get("http://localhost:8080/auth/login");
+	// WebElement username = driver.findElement(By.id("username"));
+	// username.sendKeys("o1");
+	// WebElement password = driver.findElement(By.id("password"));
+	// password.sendKeys("123");
+	// WebElement loginButton =
+	// driver.findElement(By.xpath("//button[text()='Login']"));
+	// loginButton.click();
+	// WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+	// WebElement createEventLink = wait.until(
+	// ExpectedConditions.presenceOfElementLocated(By.cssSelector("a.btn.btn-outline-success[href='/createEvent']")));
+	// createEventLink.click();
+	// WebElement bannerUpload = driver.findElement(By.name("banner"));
+	// bannerUpload.sendKeys("C:\\Users\\ACER\\Downloads\\haizz.png");
+	// WebElement posterUpload = driver.findElement(By.name("poster"));
+	// posterUpload.sendKeys("C:\\Users\\ACER\\Downloads\\haizz.png");
+	// WebElement nameInput = driver.findElement(By.id("name"));
+	// nameInput.sendKeys("Random Event Name");
+	// WebElement locationInput = driver.findElement(By.id("location"));
+	// locationInput.sendKeys("Random Location");
+	// WebElement startDateInput = driver.findElement(By.id("start_date"));
+	// startDateInput.clear();
+	// startDateInput.sendKeys("07202024\t140000");
+	// Select typeSelect = new Select(driver.findElement(By.id("type")));
+	// typeSelect.selectByValue("music");
+	// WebElement ticketSaleDateInput =
+	// driver.findElement(By.id("ticket_sale_date"));
+	// ticketSaleDateInput.clear();
+	// ticketSaleDateInput.sendKeys("07202024150000");
+	// WebElement ckEditor =
+	// driver.findElement(By.className("ck-editor__editable"));
+	// ckEditor.sendKeys("randomKeys");
+	// ckEditor.sendKeys(Keys.ENTER); // Example: Press Enter key
+	// WebElement createEventButton =
+	// driver.findElement(By.xpath("//input[@type='submit' and @class='btn
+	// btn-primary']"));
+	// ((JavascriptExecutor) driver).executeScript("arguments[0].click();",
+	// createEventButton);
+	// wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input:invalid")));
+	// WebElement invalidElement =
+	// driver.findElement(By.cssSelector("input:invalid"));
+	// String validationMessage = invalidElement.getAttribute("validationMessage");
+	// assertEquals(validationMessage, "Please fill out this field.");
+	// }
+
 	@Test
 	public void testTicketSaleDateValidation() {
 		driver.get("http://localhost:8080/auth/login");
@@ -317,8 +460,7 @@ class VieTicketSystemApplicationTests {
 		loginButton.click();
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		WebElement createEventLink = wait.until(
-				ExpectedConditions
-						.presenceOfElementLocated(By.cssSelector("a.btn.btn-outline-success[href='/createEvent']")));
+				ExpectedConditions.presenceOfElementLocated(By.cssSelector("a.btn.btn-outline-success[href='/createEvent']")));
 		createEventLink.click();
 		WebElement bannerUpload = driver.findElement(By.name("banner"));
 		bannerUpload.sendKeys("C:\\Users\\ACER\\Downloads\\haizz.png");
@@ -342,8 +484,7 @@ class VieTicketSystemApplicationTests {
 		WebElement ckEditor = driver.findElement(By.className("ck-editor__editable"));
 		ckEditor.sendKeys("randomKeys");
 		ckEditor.sendKeys(Keys.ENTER);
-		WebElement createEventButton = driver
-				.findElement(By.xpath("//input[@type='submit' and @class='btn btn-primary']"));
+		WebElement createEventButton = driver.findElement(By.xpath("//input[@type='submit' and @class='btn btn-primary']"));
 		((JavascriptExecutor) driver).executeScript("arguments[0].click();", createEventButton);
 		WebElement alertMessage = driver.findElement(By.id("alertMessage"));
 		String alertText = alertMessage.getText();
@@ -361,8 +502,7 @@ class VieTicketSystemApplicationTests {
 		loginButton.click();
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		WebElement createEventLink = wait.until(
-				ExpectedConditions
-						.presenceOfElementLocated(By.cssSelector("a.btn.btn-outline-success[href='/createEvent']")));
+				ExpectedConditions.presenceOfElementLocated(By.cssSelector("a.btn.btn-outline-success[href='/createEvent']")));
 		createEventLink.click();
 		WebElement bannerUpload = driver.findElement(By.name("banner"));
 		bannerUpload.sendKeys("C:\\Users\\ACER\\Downloads\\haizz.png");
@@ -383,8 +523,7 @@ class VieTicketSystemApplicationTests {
 		WebElement ckEditor = driver.findElement(By.className("ck-editor__editable"));
 		ckEditor.sendKeys("randomKeys");
 		ckEditor.sendKeys(Keys.ENTER); // Example: Press Enter key
-		WebElement createEventButton = driver
-				.findElement(By.xpath("//input[@type='submit' and @class='btn btn-primary']"));
+		WebElement createEventButton = driver.findElement(By.xpath("//input[@type='submit' and @class='btn btn-primary']"));
 		((JavascriptExecutor) driver).executeScript("arguments[0].click();",
 				createEventButton);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input:invalid")));
@@ -393,77 +532,4 @@ class VieTicketSystemApplicationTests {
 		assertEquals(validationMessage, "Please fill out this field.");
 	}
 
-	@Test
-	public void testEventCreation() {
-		// Open the login page
-		driver.get("http://localhost:8080/auth/login");
-
-		// Fill in username and password
-		WebElement username = driver.findElement(By.id("username"));
-		username.sendKeys("o1");
-
-		WebElement password = driver.findElement(By.id("password"));
-		password.sendKeys("123");
-
-		// Click login button
-		WebElement loginButton = driver.findElement(By.xpath("//button[text()='Login']"));
-		loginButton.click();
-
-		// Wait for the dashboard or landing page to load
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-		WebElement createEventLink = wait.until(
-				ExpectedConditions.presenceOfElementLocated(By.cssSelector("a.btn.btn-outline-success[href='/createEvent']")));
-		createEventLink.click();
-
-		// Upload Banner
-		WebElement bannerUpload = driver.findElement(By.name("banner"));
-		bannerUpload.sendKeys("C:\\Users\\ACER\\Downloads\\haizz.png"); // Replace with actual path
-
-		// Upload Poster
-		WebElement posterUpload = driver.findElement(By.name("poster"));
-		posterUpload.sendKeys("C:\\Users\\ACER\\Downloads\\haizz.png"); // Replace with actual path
-
-		// Fill in Event details
-		WebElement nameInput = driver.findElement(By.id("name"));
-		nameInput.sendKeys("Random Event Name");
-
-		WebElement locationInput = driver.findElement(By.id("location"));
-		locationInput.sendKeys("Random Location");
-
-		WebElement startDateInput = driver.findElement(By.id("start_date"));
-		startDateInput.clear();
-		startDateInput.sendKeys("07202024\t130000");
-
-		WebElement endDateInput = driver.findElement(By.id("end_date"));
-		endDateInput.clear();
-		endDateInput.sendKeys("07202024\t190000");
-
-		Select typeSelect = new Select(driver.findElement(By.id("type")));
-		typeSelect.selectByValue("music");
-
-		WebElement ticketSaleDateInput = driver.findElement(By.id("ticket_sale_date"));
-		ticketSaleDateInput.clear();
-		ticketSaleDateInput.sendKeys("07202024150000");
-
-		WebElement ckEditor = driver.findElement(By.className("ck-editor__editable"));
-		// Send keys to the CKEditor
-		ckEditor.sendKeys("randomKeys");
-
-		// Optionally, you can simulate special key actions
-		ckEditor.sendKeys(Keys.ENTER); // Example: Press Enter key
-
-		WebElement formElement = driver.findElement(By.cssSelector("form.form"));
-
-		formElement.submit();
-		// Assert the title or other elements to confirm event creation
-
-		WebElement createEventV = wait.until(
-				ExpectedConditions.presenceOfElementLocated(By.cssSelector("a.btnsd-outline-success[href='/createEvent']")));
-		createEventV.click();
-
-		String title = driver.getTitle();
-		assertTrue(title.contains("Expected Title")); // Replace with expected title
-
-		// Optionally, assert other elements or perform further validation
-	}
 }
